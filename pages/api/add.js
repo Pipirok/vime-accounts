c06d569f9974742bcb47771474f3d04d11dceb88
Pipirok <asednelalka@gmail.com>
2021-08-02 12:17:50 +0400

    Forgot to add some files, data is persistent with all users now

---
+++ pages/api/add.js
@@ -0,0 +1,28 @@
+import nc from "next-connect";
+import gun from "../../vimeGun";
+
+const handler = nc({
+  onError: function (err, req, res) {
+    res.json({ err: true, msg: "Internal server error" });
+  },
+}).post((req, res) => {
+  let { login, level } = req.body;
+
+  // Validation
+  login = login.replace(/[^A-Za-z0-9_]/g, "");
+  level = isNaN(parseInt(level)) ? 0 : parseInt(level);
+
+  if (login.length <= 0) {
+    return res.json({ err: true, msg: "Login is too short!" });
+  }
+  if (login.length >= 17) {
+    return res.json({ err: true, msg: "Login is too long!" });
+  }
+
+  let tmp = gun.get(login).put({ login, level });
+  gun.get("vimeAccs").set(tmp, () => {
+    return res.json({ err: false, msg: "Account added successfully" });
+  });
+});
+
+export default handler;
